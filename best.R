d5263c5ce9463f33f5dd4f37f4d52583cfb1f860
Craig Rowland <craigrow@hotmail.com>
2014-07-27 21:46:53 -0700

    passing tests 1-3

--- best.R
+++ best.R
@@ -4,7 +4,6 @@ best <- function(state, outcome) {
   
   ## Coerce the death rate columns to be numeric. 
   ## Heart.Attack is col 11.
-  
   data[,11] <- as.numeric(data[,11])
   
   ## Heart.Failure is col 17.
@@ -14,33 +13,36 @@ best <- function(state, outcome) {
   data[,23] <- as.numeric(data[,23])
   
   ## Check that the state and outcome are valid
-  if (outcome != "heart attack" | outcome != "heart failure" | outcome != "pneumonia"){
+  if (outcome != "heart attack" && outcome != "heart failure" && outcome != "pneumonia"){
+    stop("invalid outcome")
+  }
+  
+  if (state %in% data[,7] == FALSE) {
     stop("invalid state")
   }
-  else {
-    ## Return the hospital in the state with the lowest 30 death rate.
-    oss <- subset(data, State == state)
-    if (outcome == "heart attack"){
-      bad <- is.na(oss[,11])
-      minob <- min(oss[,11][!bad])
-      besth <- subset(oss, oss[,11] == minob)
-    }
-    
-    if (outcome == "heart failure"){
-      bad <- is.na(oss[,17])
-      min(oss[,17][!bad])
-      minob <- min(oss[,17][!bad])
-      besth <- subset(oss, oss[,17] == minob)
-    }
-    
-    if (outcome == "pneumonia"){
-      bad <- is.na(oss[,23])
-      min(oss[,23][!bad])
-      minob <- min(oss[,23][!bad])
-      besth <- subset(oss, oss[,23] == minob)
-    }
-    
-    besth[,2]
+  
+  ## Return the hospital in the state with the lowest 30 death rate.
+  oss <- subset(data, State == state)
+  if (outcome == "heart attack"){
+    bad <- is.na(oss[,11])
+    minob <- min(oss[,11][!bad])
+    besth <- subset(oss, oss[,11] == minob)
   }
+  
+  if (outcome == "heart failure"){
+    bad <- is.na(oss[,17])
+    min(oss[,17][!bad])
+    minob <- min(oss[,17][!bad])
+    besth <- subset(oss, oss[,17] == minob)
+  }
+  
+  if (outcome == "pneumonia"){
+    bad <- is.na(oss[,23])
+    min(oss[,23][!bad])
+    minob <- min(oss[,23][!bad])
+    besth <- subset(oss, oss[,23] == minob)
+  }
+  
+  besth[,2]
   
 }
