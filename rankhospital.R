56d5bdcd9ccc3919c2d2f477d3e72b83118dd28b
Craig Rowland <craigrow@hotmail.com>
2014-07-30 07:10:05 -0700

    now implemented num = 'best'

--- rankhospital.R
+++ rankhospital.R
@@ -2,6 +2,10 @@ rankhospital <- function(state, outcome, num = "best") {
   ## Read outcome data
   data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
+  if (num == "best") {
+    num <- 1
+  }
+  
   ## Coerce the death rate columns to be numeric. 
   ## Heart.Attack is col 11.
   data[,11] <- as.numeric(data[,11])
